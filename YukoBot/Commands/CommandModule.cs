6caa3858901d69ba693f183e3fa3b395e447b8bc
Hlebushek2020 <63193749+Hlebushek2020@users.noreply.github.com>
2022-01-29 11:11:58 +0300

    fixed help command for "Manage Collections" category

--- YukoBot/Commands/CommandModule.cs
+++ YukoBot/Commands/CommandModule.cs
@@ -9,7 +9,7 @@ namespace YukoBot.Commands
         #region Categories
         public static Category User { get; } = new Category("Пользовательские команды", "users");
         public static Category Management { get; } = new Category("Команды управления", "management", "Команды этой категории доступны админу гильдии (сервера) и владельцу бота");
-        public static Category CollectionManagement { get; } = new Category("Управление коллекциями", "collection - management", "Команды этой категории доступны для зарегистрированных и не забаненых(на этом сервере) пользователей");
+        public static Category CollectionManagement { get; } = new Category("Управление коллекциями", "collection-management", "Команды этой категории доступны для зарегистрированных и не забаненых (на этом сервере) пользователей");
         #endregion
 
         private static readonly Dictionary<string, Category> _categoryies = new Dictionary<string, Category>();
