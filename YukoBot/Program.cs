67f0c566f6c65f1d6b6573787380196427536189
Sergey <63193749+Hlebushek2020@users.noreply.github.com>
2022-06-07 20:33:50 +0300

    v3.1.0.1 (#11)

--- YukoBot/Program.cs
+++ YukoBot/Program.cs
@@ -1,22 +1,34 @@
-﻿using System;
+﻿using Microsoft.Extensions.Logging;
+using System;
+using YukoBot.Models.Log;
 
 namespace YukoBot
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
             Console.Title = "Yuko[Bot]";
 
             if (!YukoSettings.Availability())
             {
-                return;
+                return 0;
             }
 
-            using (YukoBot yukoBot = YukoBot.Current)
+            try
             {
-                yukoBot.RunAsync().GetAwaiter().GetResult();
+                using (YukoBot yukoBot = YukoBot.Current)
+                {
+                    yukoBot.RunAsync().GetAwaiter().GetResult();
+                }
             }
+            catch (Exception ex)
+            {
+                YukoLoggerFactory.GetInstance().CreateLogger<ServerLogger>().Log(LogLevel.Critical, "App", ex);
+                return 1;
+            }
+
+            return 0;
         }
     }
 }
