a75949731a2b2a578ce210fef98ebfa48276f589
Сергей <63193749+Hlebushek2020@users.noreply.github.com>
2022-09-06 13:48:45 +0300

    Create YukoIncorrectCommandDataException.cs

---
+++ YukoBot/Commands/Exceptions/YukoIncorrectCommandDataException.cs
@@ -0,0 +1,18 @@
+﻿using DSharpPlus.Entities;
+using System;
+
+namespace YukoBot.Commands.Exceptions
+{
+    internal class YukoIncorrectCommandDataException : Exception
+    {
+        public YukoIncorrectCommandDataException(string message) : base(message) { }
+
+        public DiscordEmbed ToDiscordEmbed(string title)
+        {
+            return new DiscordEmbedBuilder()
+                .WithTitle(title)
+                .WithColor(DiscordColor.Red)
+                .WithDescription(Message + " (⋟﹏⋞)");
+        }
+    }
+}
