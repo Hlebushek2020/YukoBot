766b89501d49179f8fc684d80b56bdbcf293ecd4
Hlebushek2020 <63193749+Hlebushek2020@users.noreply.github.com>
2023-02-18 23:53:17 +0300

    18/02/2023/23/53

--- YukoCollectionsClient/ViewModels/AuthorizationViewModel.cs
+++ YukoCollectionsClient/ViewModels/AuthorizationViewModel.cs
@@ -14,7 +14,10 @@ namespace YukoCollectionsClient.ViewModels
     public class AuthorizationViewModel : BindableBase, ICloseableView, IViewTitle
     {
         #region Propirties
-        public string Title { get => App.Name; }
+        public string Title
+        {
+            get => App.Name;
+        }
         public Action Close { get; set; }
         public string Login { get; set; }
         public Func<string> Password { get; set; }
@@ -31,18 +34,21 @@ namespace YukoCollectionsClient.ViewModels
             {
                 if (!Settings.Availability())
                 {
-                    SUI.Dialogs.MessageBox.Show("Сначало настройте программу! Значек в правом нижнем углу.", App.Name, MessageBoxButton.OK, MessageBoxImage.Warning);
+                    SUI.Dialogs.MessageBox.Show("Сначало настройте программу! Значок в правом нижнем углу.", App.Name,
+                        MessageBoxButton.OK, MessageBoxImage.Warning);
                     return;
                 }
                 if (string.IsNullOrEmpty(Login) || string.IsNullOrEmpty(Password()))
                 {
-                    SUI.Dialogs.MessageBox.Show("Все поля должны быть заполнены!", App.Name, MessageBoxButton.OK, MessageBoxImage.Warning);
+                    SUI.Dialogs.MessageBox.Show("Все поля должны быть заполнены!", App.Name, MessageBoxButton.OK,
+                        MessageBoxImage.Warning);
                     return;
                 }
                 AuthorizationResponse response = WebClient.Current.Authorization(Login, Password());
                 if (!string.IsNullOrEmpty(response.ErrorMessage))
                 {
-                    SUI.Dialogs.MessageBox.Show(response.ErrorMessage, App.Name, MessageBoxButton.OK, MessageBoxImage.Error);
+                    SUI.Dialogs.MessageBox.Show(response.ErrorMessage, App.Name, MessageBoxButton.OK,
+                        MessageBoxImage.Error);
                 }
                 else
                 {
