29286234d507f0e49bdcf6d83dce178a03072313
Sergey <63193749+Hlebushek2020@users.noreply.github.com>
2024-02-06 19:42:10 +0300

    06/02/2024/19/41

--- YukoBot/Commands/Attributes/RequireRegisteredAndNoBanAttribute.cs
+++ YukoBot/Commands/Attributes/RequireRegisteredAndNoBanAttribute.cs
@@ -12,17 +12,17 @@ namespace YukoBot.Commands.Attributes
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
     public sealed class RequireRegisteredAndNoBanAttribute : CheckBaseAttribute
     {
-        public override Task<bool> ExecuteCheckAsync(CommandContext ctx, bool help)
+        public override async Task<bool> ExecuteCheckAsync(CommandContext ctx, bool help)
         {
             ulong userId = ctx.Member.Id;
             YukoDbContext dbContext = ctx.Services.GetService<YukoDbContext>();
-            DbUser dbUser = dbContext.Users.Find(userId);
+            DbUser dbUser = await dbContext.Users.FindAsync(userId);
             if (dbUser != null)
             {
                 DbBan dbBan = dbContext.Bans.FirstOrDefault(x => x.UserId == userId && x.ServerId == ctx.Guild.Id);
-                return Task.FromResult(dbBan == null);
+                return dbBan == null;
             }
-            return Task.FromResult(false);
+            return false;
         }
     }
 }
--- YukoBot/Commands/Attributes/RequireRegisteredAttribute.cs
+++ YukoBot/Commands/Attributes/RequireRegisteredAttribute.cs
@@ -11,12 +11,12 @@ namespace YukoBot.Commands.Attributes
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
     public class RequireRegisteredAttribute : CheckBaseAttribute
     {
-        public override Task<bool> ExecuteCheckAsync(CommandContext ctx, bool help)
+        public override async Task<bool> ExecuteCheckAsync(CommandContext ctx, bool help)
         {
             ulong userId = ctx.Member.Id;
             YukoDbContext dbContext = ctx.Services.GetService<YukoDbContext>();
-            DbUser dbUser = dbContext.Users.Find(userId);
-            return Task.FromResult(dbUser != null);
+            DbUser dbUser = await dbContext.Users.FindAsync(userId);
+            return dbUser != null;
         }
     }
 }
