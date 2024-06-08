077c2cc77d9ecbdc63cbb5c2c1f5481b66f67bd8
Sergey <63193749+Hlebushek2020@users.noreply.github.com>
2024-06-09 00:32:27 +0300

    09/06/2024/00/32

--- YukoClientBase/Models/Themes/ThemeUri.cs
+++ YukoClientBase/Models/Themes/ThemeUri.cs
@@ -4,27 +4,29 @@ using System.Collections.Generic;
 namespace YukoClientBase.Models.Themes
 {
     /// <summary>
-    /// Contains constants for uniform theme resource identifiers and a method for obtaining such an identifier for a theme
+    /// Contains constants for uniform theme resource identifiers and a method for obtaining such an identifier
+    /// for a theme
     /// </summary>
-    public sealed class ThemeUri
+    public static class ThemeUri
     {
-        public static Uri Light { get; } =
-            new Uri("/Sergey.UI.Extension;component/Resources/Themes/Light.xaml", UriKind.Relative);
+        private static readonly Uri _light = new Uri(
+            "/YukoClientBase;component/Resources/Styles/Themes/Light.xaml",
+            UriKind.Relative);
 
-        public static Uri Dark { get; } =
-            new Uri("/Sergey.UI.Extension;component/Resources/Themes/Dark.xaml", UriKind.Relative);
+        private static readonly Uri _dark = new Uri(
+            "/YukoClientBase;component/Resources/Styles/Themes/Dark.xaml",
+            UriKind.Relative);
 
-        private static readonly Dictionary<Theme, Uri> _themes =
-            new Dictionary<Theme, Uri> { { Theme.Light, Light }, { Theme.Dark, Dark } };
+        private static readonly Dictionary<Theme, Uri> _themes = new Dictionary<Theme, Uri>
+        {
+            { Theme.Light, _light }, { Theme.Dark, _dark }
+        };
 
         /// <summary>
         /// Returns the <see cref="Uri"/> of the given theme
         /// </summary>
         /// <param name="theme"><see cref="Theme"/>Theme for which you need to get the Uri</param>
         /// <returns>Theme <see cref="Uri"/></returns>
-        public static Uri Get(Theme theme)
-        {
-            return _themes[theme];
-        }
+        public static Uri Get(Theme theme) => _themes[theme];
     }
 }
