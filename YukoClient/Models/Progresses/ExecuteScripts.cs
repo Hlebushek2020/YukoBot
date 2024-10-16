032c49b9e615d33c63730b867ccd2411ae8d3402
Sergey <63193749+Hlebushek2020@users.noreply.github.com>
2024-10-16 17:13:42 +0300

    Update ExecuteScripts.cs

--- YukoClient/Models/Progresses/ExecuteScripts.cs
+++ YukoClient/Models/Progresses/ExecuteScripts.cs
@@ -1,82 +1,77 @@
 ﻿using System;
 using System.Threading;
-using System.Windows;
-using System.Windows.Threading;
+using System.Threading.Tasks;
 using YukoClient.Models.Web;
 using YukoClient.Models.Web.Errors;
 using YukoClient.Models.Web.Providers;
+using YukoClientBase.Args;
 using YukoClientBase.Enums;
 using YukoClientBase.Exceptions;
 using YukoClientBase.Extensions;
-using YukoClientBase.Models.Progresses;
 using YukoClientBase.Models.Web.Responses;
-using MessageBox = YukoClientBase.Dialogs.MessageBox;
 
 namespace YukoClient.Models.Progresses
 {
-    public class ExecuteScripts : BaseProgressModel
+    public class ExecuteScripts
     {
         private readonly Server _server;
+        private readonly SynchronizationContext _synchronizationContext;
 
-        public ExecuteScripts(Server server) { _server = server; }
-
-        public override void Run(Dispatcher dispatcher, CancellationToken cancellationToken)
+        public ExecuteScripts(Server server)
         {
-            try
+            _server = server;
+            _synchronizationContext = SynchronizationContext.Current;
+        }
+
+        public async Task Run(IProgress<ProgressReportArgs> progress, CancellationToken cancellationToken)
+        {
+            progress.Report(new ProgressReportArgs { IsIndeterminate = true, Text = "Подключение" });
+            using (ExecuteScriptProvider provider = WebClient.Current.ExecuteScripts(
+                       _server.Id, _server.Scripts.Count, out Response<ExecuteScriptErrorJson> response))
             {
-                dispatcher.Invoke(() => State = "Подключение");
-                using (ExecuteScriptProvider provider = WebClient.Current.ExecuteScripts(
-                           _server.Id, _server.Scripts.Count, out Response<ExecuteScriptErrorJson> response))
+                if (response.Error != null)
                 {
-                    if (response.Error != null)
-                    {
-                        if (response.Error.Code == ClientErrorCodes.MemberBanned)
-                            throw new ClientCodeException(ClientErrorCodes.MemberBanned, response.Error.Reason);
+                    if (response.Error.Code == ClientErrorCodes.MemberBanned)
+                        throw new ClientCodeException(ClientErrorCodes.MemberBanned, response.Error.Reason);
 
-                        throw new ClientCodeException(response.Error.Code);
-                    }
-
-                    foreach (Script script in _server.Scripts)
-                    {
-                        dispatcher.Invoke(
-                            (Action<ulong, string>)((ulong channelId, string mode) =>
-                            {
-                                State = $"Выполнение правила (Канал: {channelId}; тип запроса: {mode})";
-                                script.Errors.Clear();
-                                script.CompletedWithErrors = false;
-                            }), script.Channel.Id, script.Mode.Title);
-                        provider.ExecuteScript(script);
-                        int blockCounter = 1;
-                        UrlsResponse urlsResponse = null;
-                        do
-                        {
-                            dispatcher.Invoke((Action<int>)((int block) =>
-                                State = $"Получение данных (Блок: {block})"), blockCounter);
-                            blockCounter++;
-                            urlsResponse = provider.ReadBlock();
-                            foreach (string url in urlsResponse.Urls)
-                                dispatcher.Invoke((Action<string>)((string iUrl) =>
-                                    _server.Urls.Add(iUrl)), url);
-                            if (urlsResponse.Error != null)
-                            {
-                                string errorText = urlsResponse.Error.Code.GetText(
-                                    urlsResponse.Error.Code == ClientErrorCodes.ChannelNotFound
-                                        ? urlsResponse.ChannelId
-                                        : urlsResponse.MessageId);
-                                dispatcher.Invoke(() =>
-                                {
-                                    script.Errors.Add(errorText);
-                                    script.CompletedWithErrors = true;
-                                });
-                            }
-                        } while (urlsResponse.Next);
-                    }
+                    throw new ClientCodeException(response.Error.Code);
                 }
-            }
-            catch (Exception ex)
-            {
-                dispatcher.Invoke((Action<string>)((string errorMessage) =>
-                    MessageBox.Show(errorMessage, App.Name, MessageBoxButton.OK, MessageBoxImage.Error)), ex.Message);
+
+                foreach (Script script in _server.Scripts)
+                {
+                    dispatcher.Invoke(
+                        (Action<ulong, string>)((ulong channelId, string mode) =>
+                        {
+                            State = $"Выполнение правила (Канал: {channelId}; тип запроса: {mode})";
+                            script.Errors.Clear();
+                            script.CompletedWithErrors = false;
+                        }), script.Channel.Id, script.Mode.Title);
+                    provider.ExecuteScript(script);
+                    int blockCounter = 1;
+                    UrlsResponse urlsResponse = null;
+                    do
+                    {
+                        dispatcher.Invoke((Action<int>)((int block) =>
+                            State = $"Получение данных (Блок: {block})"), blockCounter);
+                        blockCounter++;
+                        urlsResponse = provider.ReadBlock();
+                        foreach (string url in urlsResponse.Urls)
+                            dispatcher.Invoke((Action<string>)((string iUrl) =>
+                                _server.Urls.Add(iUrl)), url);
+                        if (urlsResponse.Error != null)
+                        {
+                            string errorText = urlsResponse.Error.Code.GetText(
+                                urlsResponse.Error.Code == ClientErrorCodes.ChannelNotFound
+                                    ? urlsResponse.ChannelId
+                                    : urlsResponse.MessageId);
+                            dispatcher.Invoke(() =>
+                            {
+                                script.Errors.Add(errorText);
+                                script.CompletedWithErrors = true;
+                            });
+                        }
+                    } while (urlsResponse.Next);
+                }
             }
         }
     }
