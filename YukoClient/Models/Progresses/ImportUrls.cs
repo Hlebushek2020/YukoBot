c8c5fb32e752846034d5827fae2ba8cc01010ff1
Sergey <63193749+Hlebushek2020@users.noreply.github.com>
2024-11-11 11:19:51 +0300

    11/11/2024/11/19

--- YukoClient/Models/Progresses/ImportUrls.cs
+++ YukoClient/Models/Progresses/ImportUrls.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using System.Threading;
-using System.Windows.Threading;
-using YukoClientBase.Models.Progresses;
+using System.Threading.Tasks;
+using YukoClientBase.Args;
 
 namespace YukoClient.Models.Progresses
 {
-    public class ImportUrls : BaseProgressModel
+    public class ImportUrls
     {
+        private readonly SynchronizationContext _synchronizationContext;
         private readonly ICollection<string> _urls;
         private readonly string _fileName;
 
@@ -17,23 +18,23 @@ namespace YukoClient.Models.Progresses
         {
             _urls = urls;
             _fileName = fileName;
+            _synchronizationContext = SynchronizationContext.Current;
         }
 
-        public override void Run(Dispatcher dispatcher, CancellationToken cancellationToken)
+        public Task Run(IProgress<ProgressReportArgs> progress, CancellationToken cancellationToken)
         {
-            dispatcher.Invoke(() => State = "Подготовка к импорту сылок");
+            progress.Report(new ProgressReportArgs { Text = "Подготовка к импорту ссылок" });
             using (StreamReader streamReader = new StreamReader(_fileName, Encoding.UTF8))
             {
                 while (!streamReader.EndOfStream)
                 {
                     string url = streamReader.ReadLine();
-                    dispatcher.Invoke((Action<string>)((string invokeUrl) =>
-                    {
-                        State = $"Добавление {invokeUrl}";
-                        _urls.Add(invokeUrl);
-                    }), url);
+                    progress.Report(new ProgressReportArgs { Text = $"Добавление {url}" });
+                    _synchronizationContext.Send(state => _urls.Add(url), null);
                 }
             }
+
+            return Task.CompletedTask;
         }
     }
 }
--- YukoClient/Models/Progresses/StorageInitialization.cs
+++ YukoClient/Models/Progresses/StorageInitialization.cs
@@ -4,48 +4,39 @@ using System.Collections.ObjectModel;
 using System.IO;
 using System.Text;
 using System.Threading;
-using System.Windows;
-using System.Windows.Threading;
+using System.Threading.Tasks;
 using YukoClient.Models.Web;
 using YukoClient.Models.Web.Responses;
+using YukoClientBase.Args;
 using YukoClientBase.Exceptions;
 using YukoClientBase.Models;
-using YukoClientBase.Models.Progresses;
-using MessageBox = YukoClientBase.Dialogs.MessageBox;
 
 namespace YukoClient.Models.Progresses
 {
-    public class StorageInitialization : BaseProgressModel
+    public class StorageInitialization
     {
-        public override void Run(Dispatcher dispatcher, CancellationToken cancellationToken)
+        public Task Run(IProgress<ProgressReportArgs> progress, CancellationToken cancellationToken)
         {
-            dispatcher.Invoke(() => State = "Поиск сохраненных данных");
+            progress.Report(new ProgressReportArgs { Text = "Поиск сохраненных данных" });
             string serversCacheFilePath = Path.Combine(Settings.ProgramResourceFolder, Settings.ServersCacheFile);
             if (File.Exists(serversCacheFilePath))
             {
-                dispatcher.Invoke(() => State = "Загрузка данных");
+                progress.Report(new ProgressReportArgs { Text = "Загрузка данных" });
                 string json = File.ReadAllText(serversCacheFilePath, Encoding.UTF8);
                 Storage.Current.Servers = JsonConvert.DeserializeObject<ObservableCollection<Server>>(json);
             }
             else
             {
-                try
-                {
-                    dispatcher.Invoke(() => State = "Получение данных");
-                    ServersResponse response = WebClient.Current.GetServers();
+                progress.Report(new ProgressReportArgs { Text = "Получение данных" });
+                ServersResponse response = WebClient.Current.GetServers();
 
-                    if (response.Error != null)
-                        throw new ClientCodeException(response.Error.Code);
+                if (response.Error != null)
+                    throw new ClientCodeException(response.Error.Code);
 
-                    Storage.Current.Servers = new ObservableCollection<Server>(response.Servers);
-                }
-                catch (Exception ex)
-                {
-                    dispatcher.Invoke((Action<string>)((string errorMessage) =>
-                            MessageBox.Show(errorMessage, App.Name, MessageBoxButton.OK, MessageBoxImage.Error)),
-                        ex.Message);
-                }
+                Storage.Current.Servers = new ObservableCollection<Server>(response.Servers);
             }
+
+            return Task.CompletedTask;
         }
     }
 }
