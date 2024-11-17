2b26db01d37f93aa3529a7ef447b926c3b3770e7
Sergey <63193749+Hlebushek2020@users.noreply.github.com>
2024-11-17 16:01:46 +0300

    Update UpdateServer.cs

--- YukoClient/Models/Progresses/UpdateServer.cs
+++ YukoClient/Models/Progresses/UpdateServer.cs
@@ -1,62 +1,39 @@
 ﻿using System;
 using System.Collections.ObjectModel;
-using System.Threading;
-using System.Windows;
-using System.Windows.Threading;
+using System.Threading.Tasks;
 using YukoClient.Models.Web;
 using YukoClient.Models.Web.Responses;
+using YukoClientBase.Args;
 using YukoClientBase.Enums;
 using YukoClientBase.Exceptions;
-using YukoClientBase.Models.Progresses;
 
 namespace YukoClient.Models.Progresses
 {
-    public class UpdateServer : BaseProgressModel
+    public class UpdateServer
     {
         private readonly Server _server;
 
         public UpdateServer(Server server) { _server = server; }
 
-        public override void Run(Dispatcher dispatcher, CancellationToken cancellationToken)
+        public Task Run(IProgress<ProgressReportArgs> progress)
         {
-            try
-            {
-                dispatcher.Invoke(() => State = "Получение данных о сервере");
-                ServerResponse serverResponse = WebClient.Current.GetServer(_server.Id);
+            progress.Report(new ProgressReportArgs { Text = "Получение данных о сервере" });
 
-                if (serverResponse.Error != null)
-                    throw new ClientCodeException(serverResponse.Error.Code);
+            ServerResponse serverResponse = WebClient.Current.GetServer(_server.Id);
 
-                if (!serverResponse.IconUri.Equals(_server.IconUri))
-                    _server.IconUri = serverResponse.IconUri;
+            if (serverResponse.Error != null && serverResponse.Error.Code == ClientErrorCodes.GuildNotFound)
+                throw new Exception(
+                    $"Не удалось обновить сервер \"{_server.Name}\" т.к. он не найден. Удалить его из списка?");
 
-                _server.Channels = new ObservableCollection<Channel>(serverResponse.Channels);
-            }
-            catch (Exception ex)
-            {
-                if (ex is ClientCodeException clientCodeException &&
-                    clientCodeException.ClientErrorCode == ClientErrorCodes.GuildNotFound)
-                {
-                    dispatcher.Invoke(() =>
-                    {
-                        if (MessageBox.Show(
-                                $"Не удалось обновить сервер \"{_server.Name}\" т.к. он не найден. Удалить его из списка?",
-                                App.Name,
-                                MessageBoxButton.OK,
-                                MessageBoxImage.Error) == MessageBoxResult.Yes)
-                        {
-                            if (Storage.Current.Servers.Remove(_server))
-                                Storage.Current.Save();
-                        }
-                    });
-                }
-                else
-                {
-                    dispatcher.Invoke((Action<string>)((string errorMessage) =>
-                            MessageBox.Show(errorMessage, App.Name, MessageBoxButton.OK, MessageBoxImage.Error)),
-                        ex.Message);
-                }
-            }
+            if (serverResponse.Error != null && serverResponse.Error.Code != ClientErrorCodes.GuildNotFound)
+                throw new ClientCodeException(serverResponse.Error.Code);
+
+            if (!serverResponse.IconUri.Equals(_server.IconUri))
+                _server.IconUri = serverResponse.IconUri;
+
+            _server.Channels = new ObservableCollection<Channel>(serverResponse.Channels);
+
+            return Task.CompletedTask;
         }
     }
 }
